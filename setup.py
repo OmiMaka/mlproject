5b04b783d203e64cf52de624d5d60e3c56dface0
Omikumar Makadia <om232@scarletmail.rutgers.edu>
2024-06-17 14:20:59 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.read
