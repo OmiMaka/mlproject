71534d5d9ce41eece4c5df5807b756cd74e6d6f1
Omikumar Makadia <om232@scarletmail.rutgers.edu>
2024-06-19 13:57:50 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -6,4 +6,21 @@ HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
-        requirements=file_obj.read
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Omikumar',
+author_email='om232@scarletmail.rutgers.edu',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
+
+
