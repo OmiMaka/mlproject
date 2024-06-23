b5d25b4f99f348e07c60f11d5b48137c5b2271fa
Omikumar Makadia <om232@scarletmail.rutgers.edu>
2024-06-23 18:11:37 +0530

    Pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,56 @@
+import sys
+import pandas as pd
+
+from src.exception import CustomException
+from src.utils import load_object
+
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+
+class CustomData:
+    def __init__(  self,
+        views: int,
+        comments: int,
+        duration: int):
+
+        self.views = views
+
+        self.comments = comments
+
+        self.duration = duration
+
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+
+                "views": [self.views],
+                "duration": [self.duration],
+                "comments": [self.comments],
+
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/pipeline/utils.py
+++ src/pipeline/utils.py
@@ -3,12 +3,12 @@ import sys
 
 import numpy as np 
 import pandas as pd
-import dill
+
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-from src.exception import CustomException
+from exception import CustomException
 
 def save_object(file_path, obj):
     try:
